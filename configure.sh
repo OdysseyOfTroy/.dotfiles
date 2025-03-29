5edd02ee1840698f8228f4629b931e82c63d3eab
Troy Matthews <troymatthews@Troys-MacBook-Pro.local>
2025-03-29 11:21:36 +0000

    replace nnn with neotree

--- configure.sh
+++ configure.sh
@@ -8,9 +8,6 @@ printf "$MAGENTA$BOLD%s$NORMAL\n" "🛠  Beginning tool configuration..."
 # Link dotfiles with stow
 source _scripts/stow.sh
 
-# Build and setup nnn
-source _scripts/build-nnn.sh
-
 # Install the current LTS Node version
 source _scripts/install-lts-node.sh
 
---
+++ neovim/.config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,17 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  lazy = false,
+  ---@module "neo-tree"
+  ---@type neotree.Config?
+  opts = {
+  },
+    config = function()
+        vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>")
+    end
+}
