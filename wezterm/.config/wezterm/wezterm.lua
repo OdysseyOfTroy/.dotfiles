0865cc2d22987ad4fbd031a8e25e97f3d26b6af2
Troy <TroyMatthewsHong@gmail.com>
2025-03-31 11:23:54 +0100

    minor wezterm config changes

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,8 +1,10 @@
 local wezterm = require ("wezterm")
+local mux = wezterm.mux 
 local constants = require ("constants")
 local config = wezterm.config_builder()
 
 -- font settings
+config.font_size = 16
 config.font = wezterm.font("FiraCode Nerd Font Mono")
 
 -- Colours
@@ -12,7 +14,7 @@ config.colors = {
 }
 
 -- Appearance
-config.window_decorations = 'RESIZE'
+config.window_decorations = 'TITLE|RESIZE|MACOS_FORCE_DISABLE_SHADOW'
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_image = constants.bg_image
 config.window_padding = {
@@ -24,6 +26,12 @@ config.window_padding = {
 
 -- Misc
 config.max_fps = 120
+config.prefer_egl = true
+
+wezterm.on('gui-startup', function()
+    local tab, pane, window = mux.spawn_window{}
+    window:gui_window():maximize()
+end)
 
 return config
 
