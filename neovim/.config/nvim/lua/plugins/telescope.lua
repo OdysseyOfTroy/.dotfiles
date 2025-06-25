008921e59ef80a5c72420548ff2a7529dff138f9
Troy <TroyMatthewsHong@gmail.com>
2025-06-25 13:44:05 +0100

    alter telescope appearance

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,58 @@
+local function normalise_path(path)
+    return path:gsub("\\", "/")
+end
+
+local function normalise_cwd()
+    -- lua_ls linting is incorrect, vim.loop.cwd works at runtime.
+    -- to check that cwd is working run :lua print(vim.loop.cwd())
+    return normalise_path(vim.loop.cwd()) .. "/"
+end
+
+local function is_subdirectory(cwd, path)
+  return string.lower(path:sub(1, #cwd)) == string.lower(cwd)
+end
+
+local function split_filepath(path)
+  local normalized_path = normalise_path(path)
+  local normalized_cwd = normalise_cwd()
+  local filename = normalized_path:match("[^/]+$")
+
+  if is_subdirectory(normalized_cwd, normalized_path) then
+    local stripped_path = normalized_path:sub(#normalized_cwd + 1, -(#filename + 1))
+    return stripped_path, filename
+  else
+    local stripped_path = normalized_path:sub(1, -(#filename + 1))
+    return stripped_path, filename
+  end
+end
+
+local function path_display(_, path)
+  local stripped_path, filename = split_filepath(path)
+  if filename == stripped_path or stripped_path == "" then
+    return filename
+  end
+  return string.format("%s ~ %s", filename, stripped_path)
+end
+
 return {
+    --BASE TELESCOPE--
     {
         "nvim-telescope/telescope.nvim",
         lazy = true,
         dependencies = {
             "nvim-lua/plenary.nvim",
+        },
+        opts = {
+            defaults = {
+                path_display = path_display,
+                layout_config = {
+                    horizontal = {
+                        width = { padding = 0 },
+                        height = { padding = 0 },
+                        preview_width = 0.6,
+                    },
+                },
+            },
         },
         keys = function()
             local telescope = require("telescope.builtin");
@@ -15,6 +64,7 @@ return {
            }
        end
     },
+    --TELESCOPE FOR NEOVIM CORE--
     {
         "nvim-telescope/telescope-ui-select.nvim",
         config = function()
