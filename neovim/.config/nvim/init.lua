a318fa4f13189e8f2e899ea47b56f51fb9a7e5fe
Troy <TroyMatthewsHong@gmail.com>
2025-06-25 13:44:49 +0100

    modified telescope colouration

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -17,3 +17,11 @@ vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
         vim.diagnostic.open_float(nil, {focus = false})
     end
 })
+vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "NONE", fg = "#ffffff" })
+vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "NONE", fg = "#888888" })
+vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "NONE", fg = "#ffffff" })
+vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "NONE", fg = "#888888" })
+vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = "#ffffff", bg = "NONE" })
+vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = "#ffffff", bg = "NONE" })
+vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { fg = "#ffffff", bg = "NONE" })
+vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { fg = "#ffffff", bg = "NONE" })
